65b97dcb11b97bdfe3552119fa62b80c628ee839
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2025-02-12 02:02:56 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,6 +6,9 @@ class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        self.dropdown_menu_1.selected_value = self.dropdown_menu_1.items[0]
+        self.dropdown_menu_2.selected_value = self.dropdown_menu_2.items[0]
+        self.dropdown_menu_3.selected_value = self.dropdown_menu_3.items[0]
 
         # Any code you write here will run before the form opens.
 
